6f8c0dc14e0db28b80fcdfc8b3797d532ab1dd37
Alex <alexei.strelnikov@gmail.com>
2019-10-07 14:01:26 +0300

    add 4, 5

--- test3.java
+++ test3.java
@@ -2,6 +2,7 @@ public class test3 {
     public static void main(String[] args) {
         System.out.println(2);
         System.out.println(3);
-        
+        System.out.println(4);
+        System.out.println(5);
     }
 }
